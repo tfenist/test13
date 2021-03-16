a04f5b3db260067487674bbf30b4e95bf72c527e
alexchernyh29 <55162957+alexchernyh29@users.noreply.github.com>
2021-03-16 21:24:01 +0300

    Мелкие правки дизайна, проверка bootstrap, кнопка выпадающего списка всё равно не работает

--- resources/views/news/index.blade.php
+++ resources/views/news/index.blade.php
@@ -21,43 +21,48 @@
 @section('content')
     <div class="container">
         <h3>My news</h3>
+        <table class="table">
+            <thead class="thead-light">
+            <tr>
+                <th scope="col">ID</th>
+                <th scope="col">Title</th>
+                <th scope="col">Message</th>
+                <th scope="col">Email</th>
+                <th scope="col">Actions</th>
+            </tr>
+            </thead>
+            @foreach ($news as $one_news)
+                <tbody>
+                <tr>
+                    <td>
+                        {{$one_news->id}}
+                    </td>
+                    <td>
+                        {{$one_news->subject}}
+                    </td>
+                    <td>
+                        {{$one_news->message}}
+                    </td>
+                    <td>
+                        {{$one_news->email}}
+                    </td>
+                    <td>
+                        <a href="{{route('news.destroy', ['id' => $one_news->id])}}" class="btn btn-danger"> Удалить</a>
+                        <a class="btn btn-warning" href="{{ route('news.edit' ,$one_news->id) }}">Edit</a>
+                    </td>
+                </tr>
+                </tbody>
+            @endforeach
+        </table>
         <a href="{{ route('news.create') }}" class="btn btn-success">Create</a>
-        <div class="row">
-            <div class="col-md-10 col-md-offset-1">
-                <table class="table">
-                    <thead>
-                    <tr>
-                        <td>ID</td>
-                        <td>Title</td>
-                        <td>Message</td>
-                        <td>email</td>
-                        <td>Actions</td>
-                    </tr>
-                    </thead>
-                    @foreach ($news as $one_news)
-                        <tbody>
-                        <tr>
-                            <td>
-                                {{$one_news->id}}
-                            </td>
-                            <td>
-                                {{$one_news->subject}}
-                            </td>
-                            <td>
-                                {{$one_news->message}}
-                            </td>
-                            <td>
-                                {{$one_news->email}}
-                            </td>
-                            <td>
-                                <a href="{{route('news.destroy', ['id' => $one_news->id])}}" class="btn btn-danger"> Удалить</a>
-                                <a class="btn btn-success" href="{{ route('news.edit' ,$one_news->id) }}">Edit</a>
-                            </td>
-                        </tr>
-                        </tbody>
-                    @endforeach
-                </table>
-
+        <div class="dropdown">
+            <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Кнопка выпадающего списка
+            </button>
+            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                <a class="dropdown-item" href="#">Action</a>
+                <a class="dropdown-item" href="#">Another action</a>
+                <a class="dropdown-item" href="#">Something else here</a>
             </div>
         </div>
     </div>
